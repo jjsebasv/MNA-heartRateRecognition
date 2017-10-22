8d4fcd204a75ec80624507c8504993418580d271
Sebastian Kulesz <sebikul@gmail.com>
2017-10-22 17:27:54 -0300

    Filtro de frecuencias

--- demo/heartrate.py
+++ demo/heartrate.py
@@ -9,6 +9,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import argparse
+from scipy import signal
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = signal.lfilter(b, a, data)
+    return y
 
 
 def rgb2ycbcr(im):
@@ -33,7 +48,7 @@ def main():
     #     choices=['rgb', 'ycbcr']
     # )
 
-    cap = cv2.VideoCapture('demo-video.mp4')
+    cap = cv2.VideoCapture('20171022_151444.mp4')
     # if not cap.isOpened():
     #    print("No lo pude abrir")
     #    return
@@ -53,24 +68,17 @@ def main():
     while (cap.isOpened()):
         ret, frame = cap.read()
 
-        # if k / length < 0.2:
-        #     k += 1
-        #     continue
-        #
-        # if k / length > 0.8:
-        #     break
-
         if ret == True:
-            r[0, k] = np.mean(frame[330:360, 610:640, 0])
-            g[0, k] = np.mean(frame[330:360, 610:640, 1])
-            b[0, k] = np.mean(frame[330:360, 610:640, 2])
+            r[0, k] = np.mean(frame[200:450, 490:670, 0])
+            g[0, k] = np.mean(frame[200:450, 490:670, 1])
+            b[0, k] = np.mean(frame[200:450, 490:670, 2])
 
             # r[0, k] = np.mean(frame[:, :, 0])
             # g[0, k] = np.mean(frame[:, : 1])
             # b[0, k] = np.mean(frame[:, :, 2])
 
-            # im = rgb2ycbcr(frame[330:360, 610:640, :])
-            # y[0, k] = np.mean(im[:, :, 0])
+            im = rgb2ycbcr(frame[200:450, 490:670, :])
+            y[0, k] = np.mean(im[:, :, 0])
             # cb[0, k] = np.mean(im[:, :, 0])
             # cr[0, k] = np.mean(im[:, :, 0])
             print("%g%%" % (k / length * 100.))
@@ -87,31 +95,35 @@ def main():
     r = r[0, 0:n] - np.mean(r[0, 0:n])
     g = g[0, 0:n] - np.mean(g[0, 0:n])
     b = b[0, 0:n] - np.mean(b[0, 0:n])
-    # y = y[0, 0:n] - np.mean(y[0, 0:n])
-    # cb = cb[0, 0:n] - np.mean(cb[0, 0:n])
-    # cr = cr[0, 0:n] - np.mean(cr[0, 0:n])
+    y = y[0, 0:n] - np.mean(y[0, 0:n])
+    cb = cb[0, 0:n] - np.mean(cb[0, 0:n])
+    cr = cr[0, 0:n] - np.mean(cr[0, 0:n])
+
+    yp = butter_bandpass_filter(y, 20, 180, fps*60)
 
     R = np.abs(np.fft.fftshift(np.fft.fft(r))) ** 2
     G = np.abs(np.fft.fftshift(np.fft.fft(g))) ** 2
     B = np.abs(np.fft.fftshift(np.fft.fft(b))) ** 2
-    # Y = np.abs(np.fft.fftshift(np.fft.fft(y))) ** 2
-    # CB = np.abs(np.fft.fftshift(np.fft.fft(cb))) ** 2
-    # CR = np.abs(np.fft.fftshift(np.fft.fft(cr))) ** 2
+    Y = np.abs(np.fft.fftshift(np.fft.fft(y))) ** 2
+    Yp = np.abs(np.fft.fftshift(np.fft.fft(yp))) ** 2
+    CB = np.abs(np.fft.fftshift(np.fft.fft(cb))) ** 2
+    CR = np.abs(np.fft.fftshift(np.fft.fft(cr))) ** 2
     # Y[0:30] = 0
     plt.xlabel("frecuencia [1/minuto]")
 
-    plt.plot(60 * f, R)
-    plt.xlim(0, 200)
+    # plt.plot(60 * f, R)
+    # plt.xlim(0, 200)
 
-    # plt.plot(60 * f, G)
+    # plt.plot(60 * f, R)
     # plt.xlim(0, 200)
     # # plt.legend("G")
 
     # plt.plot(60 * f, B)
     # plt.xlim(0, 200)
 
-    # plt.plot(60 * f, Y)
-    # plt.xlim(0, 200)
+    plt.plot(60 * f, Y)
+    plt.plot(60 * f, Yp)
+    plt.xlim(0, 200)
     # # plt.legend("Y")
 
     # plt.plot(60 * f, CB)
@@ -121,8 +133,8 @@ def main():
     # plt.xlim(0, 200)
 
     plt.show()
-    # print("Frecuencia cardíaca (Y): ", abs(f[np.argmax(Y)]) * 60, " pulsaciones por minuto")
-    print("Frecuencia cardíaca (G): ", abs(f[np.argmax(G)]) * 60, " pulsaciones por minuto")
+    print("Frecuencia cardíaca (Y): ", abs(f[np.argmax(Y)]) * 60, " pulsaciones por minuto")
+    print("Frecuencia cardíaca (Yp): ", abs(f[np.argmax(Yp)]) * 60, " pulsaciones por minuto")
 
 
 if __name__ == "__main__":
